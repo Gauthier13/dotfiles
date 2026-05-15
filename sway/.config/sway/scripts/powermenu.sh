9f9ad6bb5588fa8b6b443b465c2d18d19fe9a438
Gauthier13 <gauthierleclair1@outlook.fr>
2026-05-15 16:21:05 +0200

    Add power menu with fuzzel dmenu

---
+++ sway/.config/sway/scripts/powermenu.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+options="Lock\nLogout\nShutdown\nReboot"
+
+chosen=$(echo -e "$options" | fuzzel --dmenu --hide-prompt)
+
+case "$chosen" in
+    Lock)
+        swaylock
+        ;;
+    Logout)
+        swaymsg exit
+        ;;
+    Shutdown)
+	confirm=$(printf "No\nYes" | fuzzel --dmenu -p "Shutdown ?" --placeholder='' --prompt-color=f5e0dcff)
+	["$confirm" = "Yes"] && systemctl poweroff
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+esac
