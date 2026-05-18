a8092c52f7de54be1e9869117c4a093e87166073
Gauthier13 <gauthierleclair1@outlook.fr>
2026-05-18 17:32:39 +0200

    fix power menu shutdown

--- sway/.config/sway/scripts/powermenu.sh
+++ sway/.config/sway/scripts/powermenu.sh
@@ -13,9 +13,9 @@ case "$chosen" in
         ;;
     Shutdown)
 	confirm=$(printf "No\nYes" | fuzzel --dmenu -p "Shutdown ?" --placeholder='' --prompt-color=f5e0dcff)
-	["$confirm" = "Yes"] && systemctl poweroff
+	[ "$confirm" = "Yes" ] && systemctl poweroff
         ;;
     Reboot)
         systemctl reboot
-        ;;
-esac
+	    ;;
+    esac
