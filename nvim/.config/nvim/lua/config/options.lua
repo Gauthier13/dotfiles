6c987441158f7fd19d2a6525cc61782fc7ed036f
Gauthier13 <gauthierleclair1@outlook.fr>
2026-05-20 19:23:21 +0200

    Add habamax theme and delete harpoor

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,10 @@
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("habamax")
+
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.scrolloff = 10 
 vim.opt.clipboard = "unnamedplus"
+
