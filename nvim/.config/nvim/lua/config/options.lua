8e77e47b82a5969ad75f721923e8845ecd70c530
Gauthier13 <gauthierleclair1@outlook.fr>
2026-05-20 20:10:00 +0200

    add more options to nvim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -8,3 +8,32 @@ vim.opt.shiftwidth = 4
 vim.opt.scrolloff = 10 
 vim.opt.clipboard = "unnamedplus"
 
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- indent
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+--feel 
+vim.opt.colorcolumn = "80"
+vim.opt.showmatch = true
+
+--backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+
+
+
+local augroup = vim.api.nvim_create_augroup("UserConfig", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup,
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
